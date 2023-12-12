71373c2aeea3d3345b6ee611ce07575ee4b69bd9
Aakaash <84792076+Aakaashzz@users.noreply.github.com>
2023-12-12 21:24:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Index/maximum-index.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxIndexDiff(self, arr, n):
+        max_diff = 0
+        left_min = [0] * n
+        right_max = [0] * n
+
+        left_min[0] = arr[0]
+        for i in range(1, n):
+            left_min[i] = min(arr[i], left_min[i - 1])
+
+        right_max[n - 1] = arr[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(arr[i], right_max[i + 1])
+
+        i = 0
+        j = 0
+        while i < n and j < n:
+            if left_min[i] <= right_max[j]:
+                max_diff = max(max_diff, j - i)
+                j += 1
+            else:
+                i += 1
+
+        return max_diff
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+	t = int(input())
+	while(t>0):
+		num = int(input())
+		arr = [int(x) for x in input().strip().split()]
+		ob = Solution()
+		print(ob.maxIndexDiff(arr,num))
+		t-=1
+# } Driver Code Ends
