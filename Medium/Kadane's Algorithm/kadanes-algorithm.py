3984963ec5f511fe0388a6bf61a27339e9c6155b
Aakaash <84792076+Aakaashzz@users.noreply.github.com>
2023-12-27 21:42:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    def maxSubArraySum(self,arr,N):
+        max_so_far=float('-inf')
+        max_end=0
+        for num in arr:
+            max_end=max(num,max_end+num)
+            max_so_far=max(max_so_far,max_end)
+        return max_so_far
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
