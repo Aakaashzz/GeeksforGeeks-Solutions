e1951f8c4b2fa4cb21b9bd0e20e7dd3ddbc94f6b
Aakaash <84792076+Aakaashzz@users.noreply.github.com>
2024-03-05 23:23:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.py
+++ Medium/Maximum Index/maximum-index.py
@@ -1,49 +1,33 @@
 #User function Template for python3
 
 class Solution:
-    def maxIndexDiff(self, arr, n):
-        """
-        Find the maximum index difference in the given array.
-
-        Parameters:
-        - arr (List[int]): The input array.
-        - n (int): The length of the array.
-
-        Returns:
-        - int: The maximum index difference.
-        """
-        # Early return for empty array
-        if not arr:
-            return -1
-
-        # Initialize variables
-        left_min = [0] * n
-        right_max = [0] * n
-
-        # Calculate the minimum value from the left
-        left_min[0] = arr[0]
+    #Complete this function
+    # Function to find the maximum index difference.
+    def maxIndexDiff(self,a, n): 
+        ##Your code here
+        left = [0]*(n)
+        right = [0]*(n)
+        
+        left[0] = a[0]
+        right[n-1] = a[n-1]
         for i in range(1, n):
-            left_min[i] = min(left_min[i - 1], arr[i])
-
-        # Calculate the maximum value from the right
-        right_max[n - 1] = arr[n - 1]
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(right_max[i + 1], arr[i])
-
-        # Initialize variables for iterating through the arrays
+            left[i] = min(a[i], left[i-1])
+        for j in range(n-2, -1, -1):
+            right[j] = max(a[j], right[j+1])
+        
         i = 0
         j = 0
-        max_index_diff = -1
-
-        # Iterate through the arrays and find the maximum index difference
-        while i < n and j < n:
-            if left_min[i] <= right_max[j]:
-                max_index_diff = max(max_index_diff, j - i)
+        maxx = -1
+        while (i<n and j<n):
+            if right[j] >= left[i]:
+                maxx = max(maxx, j-i)
                 j += 1
             else:
                 i += 1
+        return maxx
 
-        return max_index_diff
+
+
 
 #{ 
  # Driver Code Starts
