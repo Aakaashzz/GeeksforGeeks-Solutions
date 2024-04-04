d6281b3e10fe31de71bf685f7afdfda8edceb42c
Aakaash <84792076+Aakaashzz@users.noreply.github.com>
2024-04-04 22:13:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+# User function Template for python3
+
+class Solution:
+    # Function to find sum of all possible substrings of the given string.
+    def sumSubstrings(self, s):
+        n = len(s)
+        # lookup = [0]*n
+        prev = int(s[0])
+        mod=10**9+7
+        ans=prev
+        for i in range(1, n):
+            temp=(int(s[i])*(i+1)+10*prev)%mod
+            ans=(ans+temp)%mod
+            prev=temp
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+import sys
+sys.setrecursionlimit(10**6)
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s = str(input())
+        ob=Solution()
+        print(ob.sumSubstrings(s))
+# } Driver Code Ends
